565d963122ff29866531f82062aec9fc904d3898
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-04-09 18:45:05 +0200

    first commit

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" type="text/css" href="design/default.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
